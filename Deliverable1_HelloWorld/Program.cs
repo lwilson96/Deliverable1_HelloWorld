ad3d57465f8bd1d903ed1c16038fd3b3b8c5d188
leelo <leelo@DESKTOP-9BJN52P>
2023-01-16 22:28:56 -0500

    Add project files.

---
+++ Deliverable1_HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Deliverable1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2): Hello, World!");
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+
+            Console.WriteLine("Enter the user's age: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + age);
+
+        }
+    }
+}
